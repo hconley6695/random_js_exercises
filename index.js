8df5fa445484e9f4ec159ef4efa5b63d52118486
Heather Conley <hconley6695@gmail.com>
2017-03-20 22:10:44 -0400

    random javascript exercises from udacity

---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+var number = 2;
+
+if (number % 2 === 0) {
+    console.log("even");
+} else {
+    console.log("odd");
+}
+
+// ///////////////
+
+// change the value of `musicians` to test your conditional statements
+var musicians = 0;
+
+// your code goes here
+if ( musicians > 4) {
+    console.log("this is a large group");
+} else if (musicians === 4) {
+    console.log("quartet");
+} else if (musicians === 3) {
+    console.log("trio");
+}else if (musicians === 2) {
+    console.log("duet");
+}else if (musicians === 1) {
+    console.log("solo");
+}else {
+    console.log("not a group");
+}
+
