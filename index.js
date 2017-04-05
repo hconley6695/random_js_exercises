f6c40e7de43b955cf56841af01c31fdcb5666425
Heather Conley <hconley6695@gmail.com>
2017-04-04 21:57:40 -0400

    creating objects with various properties and methods

--- index.js
+++ index.js
@@ -182,6 +182,46 @@ for (var row = 0; row < numbers.length; row++) {
 
 console.log(numbers);
 
+// ////////////////////////
+// Create an object called facebookProfile. The object should have 3 properties:
+
+// your name
+// the number of friends you have, and
+// an array of messages you've posted (as strings)
+// The object should also have 4 methods:
+
+// postMessage(message) - adds a new message string to the array
+// deleteMessage(index) - removes the message corresponding to the index provided
+// addFriend() - increases the friend count by 1
+// removeFriend() - decreases the friend count by 1
+
+var facebookProfile = {
+    name: "Heather",
+    friends: 200,
+    messages:["How's it going?", "talk to you at 5pm", "LOL"],
+    postMessage: function postMessage(message) {
+        facebookProfile.messages.push(message);
+        return facebookProfile;
+    },
+    deleteMessage: function deleteMessage(index) {
+        facebookProfile.messages.splice(index, 1);
+        return facebookProfile;
+    },
+    addFriend: function addFriend() {
+        facebookProfile.friends++;
+        return facebookProfile.friends;
+    },
+    removeFriend: function removeFriend() {
+        facebookProfile.friends--;
+        return facebookProfile.friends;
+    }
+}
+
+console.log(facebookProfile.addFriend());
+console.log(facebookProfile.removeFriend());
+console.log(facebookProfile.postMessage("What's for dinner?"));
+console.log(facebookProfile.deleteMessage(1));
+
 
 
 
